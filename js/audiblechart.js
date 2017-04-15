021690962f47b1e3d677bededed758402e7a3690
Tom Ridd <tom.ridd@methodsdigital.co.uk>
2017-04-15 17:27:45 +0100

    working chart

--- js/audiblechart.js
+++ js/audiblechart.js
@@ -6,6 +6,7 @@
 var ac = new AudioContext();
 var notes = [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4];
 var tempo = 240;
+var activePoint = 0;
 
 function playTune(tune) {
     var sequence = new AcMusic.Sequence( ac, tempo );
@@ -37,7 +38,6 @@ function drawAudibleChart() {
                 point: {
                     events: {
                         click: function () {
-
                             playNote(this.y);
                         }
                     }
@@ -46,8 +46,28 @@ function drawAudibleChart() {
         },
 
         series: [{
+            name: 'g.d.p.',
             data: notes
         }]
+    }, function(chart){
+
+        $(document).keydown(function(e){
+
+            switch(e.which) {
+                case 37:
+                    if(activePoint>0)
+                        activePoint--;
+                    break;
+                case 39:
+                    if(activePoint+1 < chart.series[0].data.length)
+                        activePoint++;
+                    break;
+            }
+            chart.tooltip.refresh(chart.series[0].data[activePoint]);
+            playNote(chart.series[0].data[activePoint].y);
+        })
+
+
     });
 }
 
